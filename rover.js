5e46280bc93744f02b0bb9143e253e184eb519c4
loganepps <logepps@gmail.com>
2024-08-10 11:25:41 -0500

    finished Test 11

--- rover.js
+++ rover.js
@@ -1,5 +1,37 @@
+const Message = require("./message.js");
+const Command = require("./command.js");
 class Rover {
-   // Write code here!
+  constructor(position, mode = "NORMAL", generatorWatts = 110) {
+    this.position = position;
+    this.mode = mode;
+    this.generatorWatts = generatorWatts;
+  }
+  receiveMessage(message) {
+    let name = message.name;
+    let results = message.commands;
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].commandType === "STATUS_CHECK") {
+        let roverStatus = {
+          mode: this.mode,
+          generatorWatts: this.generatorWatts,
+          position: this.position,
+        };
+        results = { completed: true, roverStatus: roverStatus };
+      } else if (results[i].commandType === "MODE_CHANGE") {
+        if (
+          (results[i].completed === false &&
+            results[i].value === "LOW_POWER") ||
+          "NORMAL"
+        ) {
+          this.mode = results[i].value;
+          results[i].completed === true;
+        }
+        results = { completed: true };
+      }
+    }
+    let response = { message: name, results: results };
+    return response;
+  }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -1,13 +1,55 @@
-const Rover = require('../rover.js');
-const Message = require('../message.js');
-const Command = require('../command.js');
+const Rover = require("../rover.js");
+const Message = require("../message.js");
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-
-describe("Rover class", function() {
-
-  // 7 tests here!
-
+describe("Rover class", function () {
+  // TEST 7
+  test("constructor sets position and default values for mode and generatorWatts", function () {
+    let rover = new Rover(12345, (mode = "NORMAL"), (generatorWatts = 110));
+    expect(rover).toEqual({
+      position: 12345,
+      mode: "NORMAL",
+      generatorWatts: 110,
+    });
+  });
+  // TEST 8
+  test("response returned by receiveMessage contains the name of the message", function () {
+    let rover = new Rover(54321);
+    let message = new Message("messageName", []);
+    let response = rover.receiveMessage(message);
+    expect(response.message).toBe("messageName");
+  });
+  // TEST 9
+  test("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
+    let commands = [
+      new Command("MODE_CHANGE", "LOW_POWER"),
+      new Command("STATUS_CHECK"),
+    ];
+    let rover = new Rover(67890);
+    let message = new Message("messageName", commands);
+    let response = rover.receiveMessage(message);
+    expect(response.results.length).toEqual(2);
+  });
+  // TEST 10
+  test("responds correctly to the status check command", function () {
+    let commands = [new Command("STATUS_CHECK")];
+    let rover = new Rover(12345);
+    let message = new Message("messageName", commands);
+    let response = rover.receiveMessage(message);
+    expect(response.results).toEqual({
+      completed: true,
+      roverStatus: { mode: "NORMAL", generatorWatts: 110, position: 12345 },
+    });
+  });
+  // TEST 11
+  test("responds correctly to the mode change command", function () {
+    let commands = [new Command("MODE_CHANGE", "LOW_POWER")];
+    let rover = new Rover(54321);
+    let message = new Message("messageName", commands);
+    let response = rover.receiveMessage(message);
+    expect(response.results).toEqual({ completed: true });
+  });
 });
