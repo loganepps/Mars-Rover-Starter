bfbd36210e3bf457b96b0212caac249a97d99f47
loganepps <logepps@gmail.com>
2024-08-09 21:21:42 -0500

    finished Task 2 and Task 3

--- message.js
+++ message.js
@@ -1,5 +1,13 @@
+const Command = require("./command");
+
 class Message {
-   // Write code here!
+  constructor(name, commands) {
+    this.name = name;
+    if (!name) {
+      throw Error("A name is required.");
+    }
+    this.commands = commands;
+  }
 }
 
 module.exports = Message;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,25 @@
-const Message = require('../message.js');
-const Command = require('../command.js');
+const Message = require("../message.js");
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
-
+describe("Message class", function () {
+  // TEST 4
+  test("throws error if a name is NOT passed into the constructor as the first parameter", function () {
+    expect(function () {
+      new Message();
+    }).toThrow(new Error("A name is required."));
+  });
+  // TEST 5
+  test("constructor sets name", function () {
+    let testMessage = new Message("messageName", []);
+    expect(testMessage.name).toBe("messageName");
+  });
+  // TEST 6
+  test("contains a commands array passed into the constructor as the 2nd argument", function () {
+    let commands = ["testing", 123];
+    let testMessage = new Message("messageName", commands);
+    expect(testMessage.commands).toStrictEqual(["testing", 123]);
+  });
 });
